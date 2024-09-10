cbd4b92c2cfb08dc4a9f25eebba8a8a31aad7076
Nutmegatron <cadg.toralba@gmail.com>
2024-09-10 17:41:16 +0800

    Added files for the cc2 lab 1 assignment

---
+++ CC2-Lab1-Assignment/Carlinbhert-cc2-Variables/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 5;
+        String firstName = "Carl";
+        int age = 19;
+        float myGpa = 4.0f;
+        char myGrade = 'A';
+        boolean myGraduation = false;
+
+        System.out.println("Hello, my name is " + firstName + " and I am " + age + " years old.");
+        System.out.println("I have a GPA of " + myGpa + "and my grade is " + myGrade);
+        System.out.println("Graduated? " + myGraduation);
+        System.out.println("Sum: " + (firstNumber + secondNumber));
+        System.out.println("Sum: " + (firstNumber + secondNumber));
+        System.out.println("Difference: " + (firstNumber - secondNumber));
+        System.out.println("Product: " + (firstNumber * secondNumber));
+        System.out.println("Quotient: " + (firstNumber / secondNumber));
+
+
+    }
+
+}
---
+++ CC2-Lab1-Assignment/src/main/java/Main.java
@@ -0,0 +1,24 @@
+// Laboratory Challenge #1
+// Carlinbhert Adryanne De Guzman
+//September 3, 2024
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Johnny\'s Grocery \t\t\t\t 123 ABC Avenue");
+    //used \t to create space between store title and address
+    //space added through the use of \n
+
+    // evenly spaced out items/quantities/prices
+    System.out.println("\n\nItems\t\t Quantities \t\t Prices");
+    System.out.println("Banana\t\t\t 5 \t\t\t\t $5.00");
+    System.out.println("Chicken\t\t\t 1 \t\t\t\t $6.00");
+    System.out.println("Water\t\t\t 2 \t\t\t\t $4.00");
+    System.out.println("Onion\t\t\t 1 \t\t\t\t $1.00");
+    // printed amount due at the bottom
+    System.out.println("\n\nAmount Due: \t\t\t\t\t $16.00");
+
+    System.out.println("\n\n\nThank you for shopping at Johnny\'s Grocery!");
+  }
+
+  
+}
